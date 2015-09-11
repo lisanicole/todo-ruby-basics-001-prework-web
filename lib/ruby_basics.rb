b6b9cdd2e6cee8a38df6e8913f6c04a6240bde74
lisanicole <lisanicole67@aol.com>
2015-09-11 10:53:28 -0400

    Ruby Basics, Complete

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,36 @@
 def division(num1, num2)
- 
+    puts "Dividing #{num1} / #{num2}"
+    return num1 / num2
 end
 
-def assign_variable(value)
-  
+def assign_variable(persons_name = "Bob")
+    name = persons_name
+    puts name
+    return name
 end
 
-def argue
-  
+def argue(phrase_new = "I'm right and you are wrong!")
+    puts phrase_new
+    return phrase_new
 end
 
-def greeting
-
+def greeting(greeting, name)
+    puts "#{greeting} #{name}"
+    return greeting, name
 end
 
-def return_a_value
 
+def return_a_value(phrase = "Nice")
+    return phrase
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(last_value = "expert")
+    puts last_value
+    last_value
 end
 
-def pizza_party
-
+def pizza_party(plain = "cheese", *toppings)
+    puts "#{plain}", "#{toppings}"
+    "#{plain}" "#{toppings.join}"
 end
+
